f3d78ee7840d6922ca69ad82d1f8331a661ac0a1
knjcode <knjcode@gmail.com>
2014-05-05 11:29:00 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '096c0f01d1fb9061129b6eba00cd135a24542f2447f7d0f88bb0ac6a9edbc9a32d95c249a0e6d538db6805cab15767a05135bb660c18bc5f7a0e5a558c05149c'
